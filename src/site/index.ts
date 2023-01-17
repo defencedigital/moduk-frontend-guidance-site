29d4c6b8bd3e9425d4a259a056915c764b150418
Adam L Edwards <adam@aeweb.io>
2023-01-17 14:30:02 +0000

    feat(PrimaryNav): Add PrimaryNav component

---
+++ src/site/index.ts
@@ -0,0 +1,7 @@
+// @ts-expect-error @moduk/frontend does not yet support types
+import * as mod from '@moduk/frontend/client'
+import navigation from './_includes/components/navigation'
+
+navigation()
+
+mod.initAll()
