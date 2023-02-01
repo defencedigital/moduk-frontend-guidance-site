f650bcedec7974a05077ec65395ff79ebd334542
jpveooys <66470099+jpveooys@users.noreply.github.com>
2023-02-01 10:17:26 +0000

    test(visual-regression): Restrict to main element on component pages
    This restricts the visual regression tests on the component pages to the `<main>` element. This should reduce noise and conflicts due to the side navigation changing.

--- e2e/tests/visual-regression.spec.ts
+++ e2e/tests/visual-regression.spec.ts
@@ -3,14 +3,16 @@ import { expect, test } from '@playwright/test'
 import { ALL_PAGES } from './constants'
 
 test.describe('@visual-regression', () => {
-  ALL_PAGES.forEach(({ url, screenshotName }) => {
+  ALL_PAGES.forEach(({ url, screenshotName, visualRegression: { selector } }) => {
     test.describe(url, () => {
       test.beforeEach(async ({ page }) => {
         await page.goto(url)
       })
 
       test('matches the saved screenshot', async ({ page }) => {
-        await expect(page.getByRole('document')).toHaveScreenshot([
+        const locator = selector ? page.locator(selector) : page.getByRole('document')
+
+        await expect(locator).toHaveScreenshot([
           screenshotName,
           'js-enabled.png',
         ], { scale: 'css', timeout: 20_000 })
@@ -20,7 +22,9 @@ test.describe('@visual-regression', () => {
         test.use({ javaScriptEnabled: false })
 
         test('matches the saved screenshot', async ({ page }) => {
-          await expect(page.getByRole('document')).toHaveScreenshot([
+          const locator = selector ? page.locator(selector) : page.getByRole('document')
+
+          await expect(locator).toHaveScreenshot([
             screenshotName,
             'js-disabled.png',
           ], { scale: 'css', timeout: 20_000 })
