926500296e41dcd0a8551bf29bff7c9526b82737
Adam L Edwards <adam@aeweb.io>
2023-02-07 15:36:32 +0000

    feat(Footer): Add Page

--- src/site/_includes/components/component-preview/index.ts
+++ src/site/_includes/components/component-preview/index.ts
@@ -1,3 +1,6 @@
+// @ts-expect-error iframe-resizer is missing types for v4
+import iframeResize from 'iframe-resizer/js/iframeResizer'
+
 function createTabManager(componentPreview: HTMLElement) {
   let activeTab: {
     id: string
@@ -53,7 +56,48 @@ function createTabManager(componentPreview: HTMLElement) {
   }
 }
 
+export function initComponentPreviewsIframe() {
+  const createIframe = (componentPreviewContentItem: HTMLElement) => {
+    const iframeEl = document.createElement('iframe')
+    const { exampleName, iframeId, iframeTitle } = componentPreviewContentItem.dataset
+
+    if (!exampleName || !iframeId || !iframeTitle) {
+      return null
+    }
+
+    if ('loading' in iframeEl) {
+      iframeEl.loading = 'lazy'
+    }
+
+    iframeEl.id = iframeId
+    iframeEl.style.height = `${componentPreviewContentItem.clientHeight}px`
+    iframeEl.className = componentPreviewContentItem.className
+    iframeEl.src = `preview/${exampleName}/embed.html`
+    iframeEl.title = iframeTitle
+
+    return iframeEl
+  }
+
+  const componentPreviewContentItems = Array.from(
+    document.querySelectorAll<HTMLElement>('.js-guidance-component-preview-iframe'),
+  )
+
+  componentPreviewContentItems.forEach((componentPreviewContentItem) => {
+    const iframe = createIframe(componentPreviewContentItem)
+    const parent = componentPreviewContentItem.parentElement
+    if (!iframe || !parent) {
+      return
+    }
+
+    parent.removeChild(componentPreviewContentItem)
+    parent.appendChild(iframe)
+
+    iframeResize({ autoResize: true, scrolling: 'auto' }, iframe)
+  })
+}
+
 export function initComponentPreviews() {
+  initComponentPreviewsIframe()
   const componentPreviews = Array.from(document.querySelectorAll<HTMLElement>('.js-guidance-component-preview'))
 
   componentPreviews.forEach((componentPreview) => {
