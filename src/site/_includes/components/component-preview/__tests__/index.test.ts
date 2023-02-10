b864cb14416cd9a87a73560955490f68755403f6
Adam L Edwards <adam@aeweb.io>
2023-02-10 13:53:33 +0000

    fix: Make iframe default for component previews

--- src/site/_includes/components/component-preview/__tests__/index.test.ts
+++ src/site/_includes/components/component-preview/__tests__/index.test.ts
@@ -3,7 +3,7 @@ import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { initComponentPreviewsIframe } from '..'
 
 const html = `
-  <div class="original-class js-guidance-component-preview-iframe" data-example-name="test-component" data-iframe-id="test-component--iframe" data-iframe-title="test-component-title">
+  <div class="original-class js-guidance-component-preview-iframe" data-component-name="test" data-example-name="test-component" data-iframe-id="test-component--iframe" data-iframe-title="test-component-title">
       <div class="original-class-content" >
         Template content
       </div>
@@ -33,6 +33,6 @@ describe('initComponentPreviewsIframe', () => {
     expect(iframe).toHaveClass('original-class', 'js-guidance-component-preview-iframe')
     expect(iframe).toHaveAttribute('id', 'test-component--iframe')
     expect(iframe).toHaveAttribute('title', 'test-component-title')
-    expect(iframe?.src).toBe('preview/test-component/embed.html')
+    expect(iframe?.src).toBe('/components/test/preview/test-component/embed.html')
   })
 })
--- src/site/components/preview/preview.11tydata.js
+++ src/site/components/preview/preview.11tydata.js
@@ -4,7 +4,9 @@ const { basename, dirname, join } = require('node:path')
 
 const modukFrontendLibPath = dirname(require.resolve('@moduk/frontend'))
 const paths = glob.sync(join(modukFrontendLibPath, 'nunjucks/**/__examples__/*.njk'))
-const examples = paths.map((path) => {
+const localExamples = glob.sync(join(__dirname, '../../_includes/moduk/components/**/__examples__/*.njk'))
+
+const examples = [...paths, ...localExamples].map((path) => {
   const componentName = basename(dirname(dirname(path)))
   const exampleName = basename(path, '.njk')
   const template = `moduk/components/${componentName}/__examples__/${exampleName}.njk`
