aee3a57a916792c2839557e35503be8b62aaf889
Adam L Edwards <adam@aeweb.io>
2023-02-28 17:53:41 +0000

    feat(cookie-banner): Add component

---
+++ src/site/_includes/components/cookie/cookie.ts
@@ -0,0 +1,22 @@
+export function getCookie(name: string) {
+  const cookies = document.cookie.split('; ')
+  return cookies.find((row) => row.startsWith(`${name}=`))?.split('=')[1] || null
+}
+
+export function setCookie(name: string, value: string, daysValid: number) {
+  const date = new Date()
+  date.setTime(date.getTime() + (daysValid * 24 * 60 * 60 * 1000))
+  document.cookie = `${name}=${value}; path=/; expires=${date.toUTCString()}; SameSite=Lax; Secure`
+}
+
+export function deleteCookie(name: string) {
+  if (getCookie(name)) {
+    // Cookies need to be deleted in the same level of specificity in which they were set
+    // If a cookie was set with a specified domain, it needs to be specified when deleted
+    // If a cookie wasn't set with the domain attribute, it shouldn't be there when deleted
+    // You can't tell if a cookie was set with a domain attribute or not, so try both options
+    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/`
+    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;domain=${window.location.hostname};path=/`
+    document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;domain=.${window.location.hostname};path=/`
+  }
+}
--- src/site/index.ts
+++ src/site/index.ts
@@ -1,12 +1,18 @@
 import 'core-js/actual/array/from'
+import 'core-js/actual/array/find'
+import 'core-js/actual/object/from-entries'
+import 'core-js/actual/string/starts-with'
+import 'core-js/web/url-search-params'
 // @ts-expect-error @moduk/frontend does not include type definitions
 import * as mod from '@moduk/frontend/client'
 import { initCodeSnippets } from './_includes/components/code-snippet'
 import { initComponentPreviews } from './_includes/components/component-preview'
+import { initCookieBanner } from './_includes/components/cookie'
 import { initNavigation } from './_includes/components/navigation'
 
 initCodeSnippets()
 initComponentPreviews()
+initCookieBanner()
 initNavigation()
 
 mod.initAll({
