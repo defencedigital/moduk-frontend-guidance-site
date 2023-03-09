38b2d46be42bc28c0779a8b41171354323fc0a6c
jpveooys <66470099+jpveooys@users.noreply.github.com>
2023-03-09 16:45:29 +0000

    build: Fix component previews build on Windows
    Component previews weren't working on Windows (with `glob` v8 or v9) because the Windows path separator, `\`, is an escape character in globs.
    
    This enables `windowsPathsNoEscape` in all globs as the simplest fix. This changes the meaning of `\` so it works on Windows as a path separator, but can't be used to escape special characters.

--- src/site/components/preview/preview.11tydata.js
+++ src/site/components/preview/preview.11tydata.js
@@ -3,8 +3,11 @@ const { globSync } = require('glob')
 const { basename, dirname, join } = require('node:path')
 
 const modukFrontendLibPath = dirname(require.resolve('@moduk/frontend'))
-const paths = globSync(join(modukFrontendLibPath, 'nunjucks/**/__examples__/*.njk')).sort()
-const localExamples = globSync(join(__dirname, '../../_includes/moduk/components/**/__examples__/*.njk'))
+const frontendExamplesGlob = join(modukFrontendLibPath, 'nunjucks/**/__examples__/*.njk')
+const paths = globSync(frontendExamplesGlob, { windowsPathsNoEscape: true }).sort()
+
+const localExamplesGlob = join(__dirname, '../../_includes/moduk/components/**/__examples__/*.njk')
+const localExamples = globSync(localExamplesGlob, { windowsPathsNoEscape: true })
 
 const examples = [...paths, ...localExamples].map((path) => {
   const componentName = basename(dirname(dirname(path)))
