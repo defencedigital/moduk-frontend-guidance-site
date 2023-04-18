52346c36005407c999a8b90fc4398961f8ba54a1
jpveooys <66470099+jpveooys@users.noreply.github.com>
2023-04-18 13:40:59 +0100

    test(visual-regression): Change full-page screenshot logic
    While working on #200, I had some problems with full-page screenshots in Firefox. Because we were using the document element to take the screenshot, it was scrolling to the middle of the page before taking the screenshot, causing some unexpected results with the floating back-to-top link.
    
    This switches to taking full-page screenshots using the `page` object instead. I removed the screenshot-splitting logic for now as it's no longer being used anywhere (we can always re-add it if we need it again),

--- e2e/tests/visual-regression.spec.ts
+++ e2e/tests/visual-regression.spec.ts
@@ -2,7 +2,8 @@ import { expect, test } from '@playwright/test'
 
 import { ALL_PAGES } from './constants'
 
-const MAX_HEIGHT = 30_000
+const SCREENSHOT_OPTIONS = { scale: 'css', timeout: 20_000 } as const
+
 test.describe('@visual-regression', () => {
   ALL_PAGES.forEach(({ url, screenshotName, visualRegression: { selector, skip_js_disabled: skipJsDisabled } }) => {
     test.describe(url, () => {
@@ -11,12 +12,13 @@ test.describe('@visual-regression', () => {
       })
 
       test('matches the saved screenshot', async ({ page }) => {
-        const locator = selector ? page.locator(selector) : page.getByRole('document')
+        const filenameParts = [screenshotName, 'js-enabled.png']
 
-        await expect(locator).toHaveScreenshot([
-          screenshotName,
-          'js-enabled.png',
-        ], { scale: 'css', timeout: 20_000 })
+        if (selector) {
+          await expect(page.locator(selector)).toHaveScreenshot(filenameParts, SCREENSHOT_OPTIONS)
+        } else {
+          await expect(page).toHaveScreenshot(filenameParts, { ...SCREENSHOT_OPTIONS, fullPage: true })
+        }
       })
 
       if (!skipJsDisabled) {
@@ -24,39 +26,12 @@ test.describe('@visual-regression', () => {
           test.use({ javaScriptEnabled: false })
 
           test('matches the saved screenshot', async ({ page }) => {
-            const locator = selector ? page.locator(selector) : page.getByRole('document')
+            const filenameParts = [screenshotName, 'js-disabled.png']
 
-            const boundingBox = selector ? await locator.boundingBox() : null
-
-            if (!boundingBox || MAX_HEIGHT > boundingBox.height) {
-              await expect(locator).toHaveScreenshot([
-                screenshotName,
-                'js-disabled.png',
-              ], { scale: 'css', timeout: 20_000 })
-              return
-            }
-            const remainders = boundingBox.height % MAX_HEIGHT ? [boundingBox.height % MAX_HEIGHT] : []
-
-            const heights = [
-              ...Array(Math.floor(boundingBox.height / MAX_HEIGHT)).fill(MAX_HEIGHT),
-              ...remainders,
-            ]
-            // eslint-disable-next-line no-restricted-syntax
-            for (const [index, height] of heights.entries()) {
-              // eslint-disable-next-line no-await-in-loop
-              await expect(page).toHaveScreenshot([
-                screenshotName,
-                `js-disabled-${index + 1}-of-${heights.length}.png`,
-              ], {
-                scale: 'css',
-                timeout: 20_000,
-                clip: {
-                  ...boundingBox,
-                  y: boundingBox.y + (MAX_HEIGHT * index),
-                  height,
-                },
-                fullPage: true,
-              })
+            if (selector) {
+              await expect(page.locator(selector)).toHaveScreenshot(filenameParts, SCREENSHOT_OPTIONS)
+            } else {
+              await expect(page).toHaveScreenshot(filenameParts, { ...SCREENSHOT_OPTIONS, fullPage: true })
             }
           })
         })
